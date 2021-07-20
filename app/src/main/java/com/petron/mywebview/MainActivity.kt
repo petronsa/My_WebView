490854eb250ef83842dd26c397b457ac5146ff27
Petron <androidtorrejoncillo1@gmail.com>
2021-07-20 14:54:52 +0200

    Proyecto teminado en agun emulador no van los videos raro

--- app/src/main/java/com/petron/mywebview/MainActivity.kt
+++ app/src/main/java/com/petron/mywebview/MainActivity.kt
@@ -1,26 +1,81 @@
 package com.petron.mywebview
 
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.webkit.WebChromeClient
-import android.webkit.WebViewClient
+import android.webkit.*
+import android.widget.SearchView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     //Private
     private val BASE_URL = "https://google.com"
+    private val SEARCH_PATH ="/search?q="
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
 
+        //Refresh
+
+        swipeRefresh.setOnRefreshListener {
+            webView.reload()
+        }
+
+        //Search
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+
+                p0?.let {
+                    if(URLUtil.isValidUrl(it))
+                    {
+                        // Es una url
+                        webView.loadUrl(it)
+                    }else {
+                        //No es una url
+                        webView.loadUrl("$BASE_URL$SEARCH_PATH$it")
+                    }
+                }
+                return false
+            }
+
+
+
+        })
+
         //WebView
+
 
         webView.webChromeClient = object : WebChromeClient(){
 
         }
         webView.webViewClient = object : WebViewClient(){
+
+            override fun shouldOverrideUrlLoading(view: WebView?,request: WebResourceRequest?): Boolean {
+                return false
+            }
+
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                super.onPageStarted(view, url, favicon)
+
+                searchView.setQuery(url, false)
+
+                swipeRefresh.isRefreshing = true
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+
+                swipeRefresh.isRefreshing = false
+            }
 
         }
         val setting = webView.settings
